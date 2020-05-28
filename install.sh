20ff0a8f1e0d8283fa6f5700fd644c6f14956e3f
amecea <flavius@presslabs.com>
2020-05-28 17:54:20 +0300

    Update config-connector script

--- install.sh
+++ install.sh
@@ -1,49 +1,142 @@
 #!/bin/bash
 
 CNRM_VERSION=1.7.2
+CNRM_NAME=cnrm-system
 
-if [ "$GOOGLE_CLOUD_PROJECT" == "" ]; then
-	echo "Which is the google cloud project id? "
-	read GOOGLE_CLOUD_PROJECT
-fi
+K8S_CLUSTER=
+K8S_REGION=
 
-if [ "$K8S_CLUSTER" == "" ]; then
-	  echo "Please provide the cluster name? "
-	  read K8S_CLUSTER
-fi
 
-gcloud config set project $GOOGLE_CLOUD_PROJECT
+function set_k8s_vars() {
+    local choise="$1"
+    local clusters=(${@:2})
 
-gcloud container clusters get-credentials $K8S_CLUSTER --region europe-west4 --project $GOOGLE_CLOUD_PROJECT
+    cl_pair=${clusters[$choise]}
+    cluster=(${cl_pair//:/ })
 
-# delete existing config connector
-kubectl delete sts,deploy,po,svc,roles,clusterroles,clusterrolebindings --all-namespaces -l cnrm.cloud.google.com/system=true --wait=true
-kubectl delete validatingwebhookconfiguration abandon-on-uninstall.cnrm.cloud.google.com --ignore-not-found --wait=true
-kubectl delete validatingwebhookconfiguration validating-webhook.cnrm.cloud.google.com --ignore-not-found --wait=true
-kubectl delete mutatingwebhookconfiguration mutating-webhook.cnrm.cloud.google.com --ignore-not-found --wait=true
+    K8S_CLUSTER=${cluster[0]}
+    K8S_REGION=${cluster[1]}
+}
 
-gcloud iam service-accounts create cnrm-system
+function get_cluster() {
+    echo "Determine cluster ..."
+    # list all clusters from current project to the user
+    raw_clusters=$(gcloud container clusters list -q  2>/dev/null)
+    echo "$raw_clusters"
 
-gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT \
---member="serviceAccount:cnrm-system@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com" \
---role="roles/owner"
+    clusters=($(echo -e "$raw_clusters" | awk 'FNR > 1 {print $1 ":" $2 }' ))
 
-gcloud iam service-accounts add-iam-policy-binding cnrm-system@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com \
---member="serviceAccount:$GOOGLE_CLOUD_PROJECT.svc.id.goog[cnrm-system/cnrm-controller-manager]" \
---role="roles/iam.workloadIdentityUser"
+    if [ "${#clusters[@]}" -eq 1 ] ; then
+        set_k8s_vars 0 "${clusters[@]}"
+    fi
 
-gsutil cp gs://cnrm/$CNRM_VERSION/release-bundle.tar.gz release-bundle.tar.gz
+    count=0
+    echo -e "\nCHOISE    NAME  \tZONE"
+    for cluster_pair in ${clusters[@]}; do
+        cluster=(${cluster_pair//:/ })
+        echo -e "$count   ->    ${cluster[0]}  ${cluster[1]}"
+        let count++ 1
+    done
 
-tar zxvf release-bundle.tar.gz
+    choise=0
 
-sed -i.bak "s/\${PROJECT_ID?}/$project_id/" install-bundle-workload-identity/0-cnrm-system.yaml
+    echo -en "\nEnter your choise for cluster [$choise] (${cluster_pair[$choise]}): "
+    read  choise
 
-kubectl apply -f install-bundle-workload-identity/
+    # check the read value is a number
+    if ! [ -n "$choise" ] || ! [ "$choise" -eq "$choise" ] 2>/dev/null ; then
+        choise=0
+    fi
 
-kubectl wait -n cnrm-system \
- --for=condition=Ready pod --all
+    set_k8s_vars $choise ${clusters[@]}
+}
 
-# cleanup
-rm -rf $PWD
+# uninstall config connector
+function uninstall() {
+    echo "Uninstall old version ..."
 
-exit
+    DELETE="kubectl delete --all-namespaces --ignore-not-found --wait=true"
+    $DELETE sts,deploy,po,svc,roles,clusterroles,clusterrolebindings -l cnrm.cloud.google.com/system=true
+    $DELETE validatingwebhookconfiguration abandon-on-uninstall.cnrm.cloud.google.com
+    $DELETE validatingwebhookconfiguration validating-webhook.cnrm.cloud.google.com
+    $DELETE mutatingwebhookconfiguration mutating-webhook.cnrm.cloud.google.com
+}
+
+
+function configure_iam() {
+    echo "Configure IAM ..."
+    # create cnrm-system account if created this may fail
+    gcloud iam service-accounts create $CNRM_NAME || true
+
+    # give owner permissions on your project
+    gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT \
+           --member="serviceAccount:${CNRM_NAME}@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com" \
+           --role="roles/owner"
+
+    # IAM policy binding between the IAM Service Account and the predefined Kubernetes service account
+    gcloud iam service-accounts add-iam-policy-binding $CNRM_NAME@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com \
+           --member="serviceAccount:$GOOGLE_CLOUD_PROJECT.svc.id.goog[${CNRM_NAME}/cnrm-controller-manager]" \
+           --role="roles/iam.workloadIdentityUser"
+}
+
+
+function install() {
+    echo "Install Config Connector ..."
+    # temporary directory
+    mkdir tmp
+    cd tmp
+
+    # download manifests
+    gsutil cp gs://cnrm/$CNRM_VERSION/release-bundle.tar.gz release-bundle.tar.gz
+
+    # extract
+    tar zxvf release-bundle.tar.gz 1>/dev/null
+
+    # Provide your project ID in the controller's installation manifest
+    sed -i.bak "s/\${PROJECT_ID?}/$project_id/" install-bundle-workload-identity/0-cnrm-system.yaml
+
+    # apply manifests
+    kubectl apply -f install-bundle-workload-identity/
+
+    # cleanup
+    cd ..
+    rm -rf tmp
+}
+
+function check_installation() {
+    echo "Check installation ..."
+    kubectl wait -n cnrm-system \
+            --for=condition=Ready pod --all
+}
+
+function main() {
+    set -e
+
+    if [ "$GOOGLE_CLOUD_PROJECT" == "" ]; then
+	      echo "Which is the google cloud project id? "
+	      read GOOGLE_CLOUD_PROJECT
+    fi
+
+    # set project or make sure that the project is set
+    gcloud config set project $GOOGLE_CLOUD_PROJECT
+
+    # fetch values for K8S_CLUSTER and K8S_REGION
+    get_cluster
+
+    gcloud container clusters get-credentials $K8S_CLUSTER --region $K8S_REGION --project $GOOGLE_CLOUD_PROJECT
+
+    # make sure that config connector is not installed
+    uninstall
+
+    # Setting up the identity
+    configure_iam
+
+    # install
+    install
+
+    # check installation
+    check_installation
+}
+
+
+main
